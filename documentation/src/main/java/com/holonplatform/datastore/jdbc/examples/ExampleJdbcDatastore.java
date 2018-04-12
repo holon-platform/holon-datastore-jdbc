be1db002ad4a038c4a63bdd70f6a576ebc99eece
Riccardo Righi <rr9379@gmail.com>
2018-04-12 16:54:12 +0200

    5.1.0 documentation updates

--- documentation/src/main/java/com/holonplatform/datastore/jdbc/examples/ExampleJdbcDatastore.java
+++ documentation/src/main/java/com/holonplatform/datastore/jdbc/examples/ExampleJdbcDatastore.java
@@ -15,17 +15,27 @@
  */
 package com.holonplatform.datastore.jdbc.examples;
 
+import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.List;
+import java.util.stream.Stream;
 
+import javax.persistence.Column;
+import javax.persistence.Id;
 import javax.sql.DataSource;
 
+import com.holonplatform.core.Validator;
+import com.holonplatform.core.beans.BeanPropertySet;
 import com.holonplatform.core.datastore.DataTarget;
 import com.holonplatform.core.datastore.Datastore;
 import com.holonplatform.core.datastore.DatastoreConfigProperties;
 import com.holonplatform.core.datastore.DefaultWriteOption;
+import com.holonplatform.core.property.NumericProperty;
 import com.holonplatform.core.property.PathProperty;
 import com.holonplatform.core.property.PropertyBox;
+import com.holonplatform.core.property.PropertySet;
+import com.holonplatform.core.property.StringProperty;
 import com.holonplatform.core.query.QueryFilter;
 import com.holonplatform.core.query.QuerySort;
 import com.holonplatform.datastore.jdbc.JdbcDatastore;
@@ -33,8 +43,10 @@ import com.holonplatform.datastore.jdbc.OrderBySort;
 import com.holonplatform.datastore.jdbc.WhereFilter;
 import com.holonplatform.datastore.jdbc.composer.SQLDialect;
 import com.holonplatform.datastore.jdbc.composer.SQLDialectContext;
+import com.holonplatform.datastore.jdbc.config.IdentifierResolutionStrategy;
 import com.holonplatform.jdbc.DataSourceConfigProperties;
 import com.holonplatform.jdbc.DatabasePlatform;
+import com.holonplatform.jdbc.JdbcConnectionHandler;
 
 @SuppressWarnings("unused")
 public class ExampleJdbcDatastore {
@@ -43,7 +55,8 @@ public class ExampleJdbcDatastore {
 	private class MyDialect implements SQLDialect {
 
 		@Override
-		public void init(SQLDialectContext context) throws SQLException {}
+		public void init(SQLDialectContext context) throws SQLException {
+		}
 
 		@Override
 		public boolean supportsLikeEscapeClause() {
@@ -60,29 +73,171 @@ public class ExampleJdbcDatastore {
 			return false;
 		}
 
-
 	}
 
-	public void setup() {
-		// tag::setup[]
-		JdbcDatastore datastore = JdbcDatastore.builder() // obtain the builder
-				.dataSource(getDataSource()) // <1>
-				.database(DatabasePlatform.ORACLE) // <2>
+	public void builder1() {
+		// tag::builder1[]
+		JdbcDatastore datastore = JdbcDatastore.builder() // <1>
+				// Datastore configuration omitted
 				.build();
-
-		datastore = JdbcDatastore.builder() // obtain the builder
-				.dataSource(DataSourceConfigProperties.builder().withPropertySource("jdbc.properties").build()) // <3>
-				.dialect(new MyDialect()) // <4>
-				.build();
-
-		datastore = JdbcDatastore.builder() // obtain the builder
-				.dataContextId("db1") // <5>
-				.dataSource(DataSourceConfigProperties.builder("db1").withPropertySource("jdbc.properties").build()) // <6>
-				.configuration(
-						DatastoreConfigProperties.builder("db1").withPropertySource("datastore.properties").build()) // <7>
-				.build();
-		// end::setup[]
+		// end::builder1[]
 	}
+
+	public void builder2() {
+		// tag::builder2[]
+		Datastore datastore = JdbcDatastore.builder() // <1>
+				// Datastore configuration omitted
+				.build();
+		// end::builder2[]
+	}
+
+	public void setup1() {
+		// tag::setup1[]
+		Datastore datastore = JdbcDatastore.builder()
+				// DataSource configuration omitted
+				.dataContextId("mydataContextId") // <1>
+				.traceEnabled(true) // <2>
+				.build();
+		// end::setup1[]
+	}
+
+	public void setup2() {
+		// tag::setup2[]
+		Datastore datastore = JdbcDatastore.builder()
+				// DataSource configuration omitted
+				.configuration(DatastoreConfigProperties.builder().withPropertySource("datastore.properties").build()) // <1>
+				.build();
+		// end::setup2[]
+	}
+
+	public void setup3() {
+		// tag::setup3[]
+		DataSource datasource = createOrObtainDatasource();
+
+		Datastore datastore = JdbcDatastore.builder() //
+				.dataSource(datasource) // <1>
+				.build();
+		// end::setup3[]
+	}
+
+	public void setup4() {
+		// tag::setup4[]
+		Datastore datastore = JdbcDatastore.builder() //
+				.dataSource(DataSourceConfigProperties.builder().withPropertySource("datasource.properties").build()) // <1>
+				.build();
+		// end::setup4[]
+	}
+
+	public void setup5() {
+		// tag::setup5[]
+		Datastore datastore = JdbcDatastore.builder() //
+				.dataSource(createOrObtainDatasource()) //
+				.database(DatabasePlatform.H2) // <1>
+				.build();
+		// end::setup5[]
+	}
+
+	public void setup6() {
+		// tag::setup6[]
+		Datastore datastore = JdbcDatastore.builder() //
+				.dataSource(createOrObtainDatasource()) //
+				.dialect(SQLDialect.h2()) // <1>
+				.dialect("com.holonplatform.datastore.jdbc.composer.dialect.H2Dialect") // <2>
+				.build();
+		// end::setup6[]
+	}
+
+	public void setup7() {
+		// tag::setup7[]
+		Datastore datastore = JdbcDatastore.builder() //
+				.dataSource(createOrObtainDatasource()) //
+				.configuration(DatastoreConfigProperties.builder().withPropertySource("datastore.properties").build()) // <1>
+				.build();
+		// end::setup7[]
+	}
+
+	public void setup8() {
+		// tag::setup8[]
+		Datastore datastore = JdbcDatastore.builder() //
+				.dataSource(createOrObtainDatasource()) //
+				.identifierResolutionStrategy(IdentifierResolutionStrategy.TABLE_PRIMARY_KEY) // <1>
+				.build();
+		// end::setup8[]
+	}
+
+	public void setup9() {
+		// tag::setup9[]
+		Datastore datastore = JdbcDatastore.builder() //
+				.dataSource(createOrObtainDatasource()) //
+				.connectionHandler(new JdbcConnectionHandler() { // <1>
+
+					@Override
+					public Connection getConnection(DataSource dataSource, ConnectionType connectionType)
+							throws SQLException {
+						// provide the JDBC connection
+						return dataSource.getConnection();
+					}
+
+					@Override
+					public void releaseConnection(Connection connection, DataSource dataSource,
+							ConnectionType connectionType) throws SQLException {
+						// release the JDBC connection
+						connection.close();
+					}
+
+				}).build();
+		// end::setup9[]
+	}
+
+	// tag::naming1[]
+	static final NumericProperty<Long> ID = NumericProperty.longType("code") // <1>
+			.validator(Validator.notNull());
+	static final StringProperty VALUE = StringProperty.create("text") // <2>
+			.validator(Validator.max(100));
+
+	static final PropertySet<?> TEST = PropertySet.builderOf(ID, VALUE).identifier(ID).build(); // <3>
+
+	static final DataTarget<?> TARGET = DataTarget.named("test"); // <4>
+	// end::naming1[]
+
+	public void naming2() {
+		// tag::naming2[]
+		Datastore datastore = JdbcDatastore.builder().dataSource(createOrObtainDatasource()).build(); // <1>
+
+		PropertyBox value = PropertyBox.builder(TEST).set(ID, 1L).set(VALUE, "One").build();
+		datastore.save(TARGET, value); // <2>
+
+		Stream<PropertyBox> results = datastore.query().target(TARGET).filter(ID.goe(1L)).stream(TEST); // <3>
+
+		List<String> values = datastore.query().target(TARGET).sort(ID.asc()).list(VALUE); // <4>
+
+		datastore.bulkDelete(TARGET).filter(ID.gt(0L)).execute(); // <5>
+		// end::naming2[]
+	}
+
+	public void mapping() {
+		// tag::mapping[]
+		StringProperty PROPERTY = StringProperty.create("propertyName").dataPath("str"); // <1>
+		// end::mapping[]
+	}
+
+	static
+	// tag::mapping2[]
+	class MyEntity {
+
+		public static final BeanPropertySet<MyEntity> PROPERTIES = BeanPropertySet.create(MyEntity.class);
+
+		@Id
+		@Column(name = "CODE")
+		private Long id;
+
+		@Column(name = "VALUE")
+		private String name;
+
+		// getters and setters omitted
+
+	}
+	// end::mapping2[]
 
 	public void connection() {
 		// tag::connection[]
@@ -122,6 +277,10 @@ public class ExampleJdbcDatastore {
 		// tag::orderby[]
 		QuerySort sort = OrderBySort.create("id asc, name desc"); // <1>
 		// end::orderby[]
+	}
+
+	private static DataSource createOrObtainDatasource() {
+		return null;
 	}
 
 	@SuppressWarnings("static-method")
